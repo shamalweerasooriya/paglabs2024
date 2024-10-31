6cc80f235542087e1be8dc243673dd95aaa4e66c
Shashiniw <58620099+shashiniw@users.noreply.github.com>
2024-10-31 11:46:04 +0530

    fbs benchmark+schema changed (#2)

---
+++ src/main/scala/helpers/Constants.scala
@@ -0,0 +1,15 @@
+package helpers
+
+object Constants extends Enumeration {
+  val fname = "John"
+  val age = 30
+  val lname = "Doe"
+  val gender = "female"
+  val email = "john@email.com"
+  val phone = "07117832842"
+  val street = "ABC street";
+  val city = "XYZ";
+  val state = "ABCD";
+  val postalCode = "27432";
+  val country = "USA";
+}
--- src/main/scala/helpers/FlatbufferHelper.scala
+++ src/main/scala/helpers/FlatbufferHelper.scala
@@ -1,6 +1,6 @@
 package helpers
 
-import MyNamespace.Person
+import MyNamespace.{Address, Person}
 import com.google.flatbuffers.FlatBufferBuilder
 
 import java.nio.ByteBuffer
@@ -10,13 +10,28 @@ class FlatbufferHelper {
     val builder = new FlatBufferBuilder(1024)
 
     // Create a FlatBuffer object for `Person`
-    val name = builder.createString("John Doe")
-    val age = builder.createString("30")
+    val f_name = builder.createString(Constants.fname)
+    val l_name = builder.createString(Constants.lname)
+    val city = builder.createString(Constants.city)
+    val state = builder.createString(Constants.state)
+    val street = builder.createString(Constants.street)
+    val country = builder.createString(Constants.country)
+    val postalcode = builder.createString(Constants.postalCode)
+    val gender = builder.createString(Constants.gender)
+    val email = builder.createString(Constants.email)
+    val phone = builder.createString(Constants.phone)
+
+    val address = Address.createAddress(builder, street, city, state, postalcode, country)
 
     // Start building the `Person` table
     Person.startPerson(builder)
-    Person.addName(builder, name)
-    Person.addAge(builder, age)
+    Person.addFirstName(builder, f_name)
+    Person.addLastName(builder, l_name)
+    Person.addAge(builder, Constants.age)
+    Person.addEmail(builder, email)
+    Person.addGender(builder, gender)
+    Person.addAddress(builder, address)
+
     val personOffset = Person.endPerson(builder)
 
     // Complete the buffer
@@ -27,7 +42,7 @@ class FlatbufferHelper {
   def Read(payload: Array[Byte]): String = {
     // Retrieve the data
     val person = Person.getRootAsPerson(ByteBuffer.wrap(payload))
-    person.name() + " is " + person.age() + " years old"
+    person.firstName() + " " + person.lastName() + " is " + person.age() + " years old. Lives at " + person.address().country()
   }
 
 }
