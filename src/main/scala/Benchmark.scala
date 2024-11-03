84803d98ed591754ee8b7f4e6a7ca388104c9cf6
jsminiSS <jamini.samarathunge@pagero.com>
2024-11-03 21:47:21 +0530

    Protobuff intergration

--- src/main/scala/Benchmark.scala
+++ src/main/scala/Benchmark.scala
@@ -1,5 +1,5 @@
 import cats.effect.{IO, IOApp}
-import helpers.FlatbufferHelper
+import helpers.{FlatbufferHelper, ProtobuffHelper}
 
 object Benchmark extends IOApp.Simple {
 
@@ -14,6 +14,14 @@ object Benchmark extends IOApp.Simple {
       _ <- IO(println(s"Total time taken for Accessing = $decodeTime ms"))
       avgSize = encodedMessages.flatten.length / Iterations
       _ <- IO(println(s"Wire format size = $avgSize bytes"))
+
+      encodeTimeProtobuf <- IO(encodingTimeProtoBuf)
+      _ <- IO(println(s"Total time taken for Serialization of Protobuf = $encodeTimeProtobuf ms"))
+      decodeTime <- IO(decodingTimeProtoBuf)
+      _ <- IO(println(s"Total time taken for Accessing of Protobuf = $decodeTime ms"))
+      avgSize = encodedMessages.flatten.length / Iterations
+      _ <- IO(println(s"Wire format size = $avgSize bytes"))
+
     } yield ()
   }
 
@@ -23,10 +31,24 @@ object Benchmark extends IOApp.Simple {
     System.currentTimeMillis() - startTime
   }
 
+  private def encodingTimeProtoBuf: Long = {
+    val startTime = System.currentTimeMillis()
+    encodedMessages = Array.fill(Iterations)(ProtobuffHelper().Encode())
+    System.currentTimeMillis() - startTime
+  }
+
   private def decodingTime: Long = {
     val startTime = System.currentTimeMillis()
     encodedMessages.foreach { payload =>
       if (payload.nonEmpty) FlatbufferHelper().Read(payload)
+    }
+    System.currentTimeMillis() - startTime
+  }
+
+  private def decodingTimeProtoBuf: Long = {
+    val startTime = System.currentTimeMillis()
+    encodedMessages.foreach { payload =>
+      if (payload.nonEmpty) ProtobuffHelper().Read(payload)
     }
     System.currentTimeMillis() - startTime
   }
--- src/main/scala/helpers/ProtobuffHelper.scala
+++ src/main/scala/helpers/ProtobuffHelper.scala
@@ -1,15 +1,17 @@
 package helpers
 
-import person.Person
+import person.{Address, Person}
 
 class ProtobuffHelper {
   def Encode(): Array[Byte] = {
-    val person = Person(name = "John Doe", age = "30", unknownFields = _root_.scalapb.UnknownFieldSet.empty)
+    val person = Person(firstName = Constants.fname, lastName = Constants.lname, age = Constants.age, gender = Constants.gender,
+      email = Constants.email, phone = Constants.phone, address = Option(Address(street = Constants.street, city = Constants.city, state = Constants.state,
+        postalCode = Constants.postalCode, country = Constants.country)))
     person.toByteArray
   }
 
   def Read(buf: Array[Byte]): String = {
     val person = Person.parseFrom(buf)
-    s"${person.name} is ${person.age} years old."
+    s"${person.firstName} is ${person.age} years old."
   }
 }
